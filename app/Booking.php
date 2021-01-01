fe54f39b63576dc3a655c91f16a420b81026eac1
Chibikuro777 <sanae.kawasaka@gmail.com>
2021-01-01 14:39:39 +0900

    modified fillable -Booking.php-

--- app/Booking.php
+++ app/Booking.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     protected $fillable = [
+        'id',
+        'created_at',
         'date',
         'time',
         'people',
